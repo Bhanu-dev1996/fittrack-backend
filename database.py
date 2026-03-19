99b7f4d3ccdf3bceda871c45e7cd405816a29315
Bhanu Shankar <113974500+Bhanu-dev1996@users.noreply.github.com>
2026-03-19 14:56:25 +0530

    cloud db

--- database.py
+++ database.py
@@ -1,9 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:bhanu123@localhost:5432/fittrack_app"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
